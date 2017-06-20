43d70cfef7cec458bbda1a7a0a5768ab28cba2af
ltasneem <lubaba070@gmail.com>
2017-06-20 15:34:17 -0400

    binary to decimal converter

---
+++ README.md
@@ -0,0 +1,12 @@
+
+Create a Java application which will accept as input a binary number and return as output the decimal value of that number. Do not use built-in functions. You must loop through the binary number and calculate the value of each position.
+
+Format the output as follows:
+The decimal value of 10011100 is 156
+
+Test data:
+01100111 = 103
+00000000 = 0
+11111111 = 255
+11110111 = 247
+
---
+++ src/Bin2Dec.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class Bin2Dec {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String binary;
+		int sum=0;
+		Scanner keyboard = new Scanner(System.in);
+		binary = keyboard.next();
+		
+		char [] array = binary.toCharArray();
+		int j=0;
+		
+		for(int i=array.length-1; i>= 0;i--)
+		{
+			if(array[i] == '1')
+			{
+				sum+= Math.pow(2, j);
+			}
+			j++;
+		}
+		
+		System.out.println("The decimal value of "+ binary+ " is "+ sum);
+		 
+
+	}
+
+}
